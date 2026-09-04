31e7b8e4c715e9f0b5fab66f1f839f9a30e58912
Evelyn Yee <66496127+evelynyee27@users.noreply.github.com>
2026-09-04 14:57:09 -0400

    commented out react import

--- src/StudyTimer.tsx
+++ src/StudyTimer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+//import React, { useState, useEffect } from "react";
 import Sky from "./components/Sky.tsx";
 import Button from "./components/Button.tsx";
 
